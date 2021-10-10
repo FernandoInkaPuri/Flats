fb3aad64b8be1d6fd673c7f293af118be038a7ca
Luiz Fernando <luizfegomez@gmail.com>
2021-10-10 20:12:53 -0300

    Começando a autenticar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :property_owners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  resources :properties
+  resources :properties do 
+    get 'my_properties', on: :collection
+  end
+
   resources :property_types
   resources :property_locations
 
