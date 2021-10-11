5c2b28479806c5f81ccd41a3d612e64a457a07a9
Luiz Fernando <luizfegomez@gmail.com>
2021-10-11 01:34:10 -0300

    Dois tipos de usuários diferentes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #devise cria varias rotas para o model que vai usá-lo
   devise_for :property_owners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
